ace1304031efb536594ec51f1f86cdd413a16226
Lucero <75174023@certus.edu.pe>
2021-12-01 15:01:42 -0500

    login/register

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { mongodb } = require('./keys');
 
 mongoose.connect(mongodb.URI, {useNewUrlParser: true })
-    .then(db => console.log('Database conected'))
+    .then(db => console.log('Base de datos conectada'))
     .catch(err => console.error(err));
 
    
